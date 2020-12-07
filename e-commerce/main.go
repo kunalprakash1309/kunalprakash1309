eb0e4699d3e5a5201441104b397c53f4db6a341c
kunal prakash <kunalprakash1309@gmail.com>
2020-12-08 02:09:48 +0530

    Delete user

--- e-commerce/main.go
+++ e-commerce/main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"net/http"
-	"log"
 	"encoding/json"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
-	// mgo "gopkg.in/mgo.v2"
+	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
+// DB struct to initialize the database
+type DB struct {
+	Database *mgo.Database
+}
+
 // User to hold the users data
-// work on err 
-// means it sends output while i provide different data type on POST
 type User struct {
-	ID        bson.ObjectId    `json:"id" bson:"_id,omitempty"`
-	Email     string `json:"email" bson:"email"`
-	FirstName string `json:"firstName" bson:"firstName"`
-	LastName  string `json:"lastName" bson:"lastName"`
-	Password  string `json:"password" bson:"password"`
-	Address []Address `json:"address" bson:"address"`
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Email     string        `json:"email" bson:"email"`
+	FirstName string        `json:"firstName" bson:"firstName"`
+	LastName  string        `json:"lastName" bson:"lastName"`
+	Password  string        `json:"password,omitempty" bson:"password,omitempty"`
+	Address   []Address     `json:"address" bson:"address"`
 }
 
 // Address struct to hold the address data
@@ -29,10 +32,38 @@ type Address struct {
 	Sector string `json:"sector" bson:"sector"`
 }
 
-func see(w http.ResponseWriter, r *http.Request){
+// GetUser to get the user using the user ID
+func (db *DB) GetUser(w http.ResponseWriter, r *http.Request) {
+	// get all parameters in form of map
+	vars := mux.Vars(r)
+
+	var user User
+
+	collection := db.Database.C("users")
+	statement := bson.M{"_id": bson.ObjectIdHex(vars["id"])}
+	err := collection.Find(statement).One(&user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("No Such User"))
+		// log.Fatalln("error in retrieving the data")
+	} else {
+		w.WriteHeader(http.StatusOK)
+		response, err := json.Marshal(user)
+		if err != nil {
+			w.Write([]byte("error in converting struct to json "))
+			log.Fatalln("error in converting struct to json ", err)
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write(response)
+	}
+
+}
+
+// PostUser to post the user into database
+func (db *DB) PostUser(w http.ResponseWriter, r *http.Request) {
 
 	contentType := r.Header.Get("content-type")
-	log.Println(contentType)
 
 	if contentType == "application/json" {
 		var user User
@@ -41,17 +72,26 @@ func see(w http.ResponseWriter, r *http.Request){
 		if err != nil {
 			log.Println("error in encoding the post body", err)
 		}
-	
+
 		// store id
 		user.ID = bson.NewObjectId()
 		
-		w.Header().Set("Content-type", "application/json")
-		response, err := json.Marshal(user)
+		collection := db.Database.C("users")
+		err = collection.Insert(user)
 		if err != nil {
-			log.Println("error in convertinf struct to json", err)
-		}
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Cannot insert user into database"))
+
+			log.Fatalln(err.Error())
+		} else {
+			w.Header().Set("Content-type", "application/json")
+			response, err := json.Marshal(user)
+			if err != nil {
+				log.Println("error in converting struct to json", err)
+			}
 	
-		w.Write(response)
+			w.Write(response)
+		}
 	} else {
 		w.WriteHeader(http.StatusNotAcceptable)
 		w.Write([]byte("unsupported format"))
@@ -59,13 +99,41 @@ func see(w http.ResponseWriter, r *http.Request){
 
 }
 
+// DeleteUser to delete user data from database
+func (db *DB) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	collection := db.Database.C("users")
+	statement := bson.M{"_id": bson.ObjectIdHex(vars["id"])}
+
+	err := collection.Remove(statement)
+	if err != nil {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("Content-type", "text/plain")
+		w.Write([]byte("Deleted Succesfully!"))
+	}
+}
 
 func main() {
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		log.Println("session creatin error", err)
+	}
+
+	database := session.DB("Ecommerce")
+	db := &DB{
+		Database: database,
+	}
+	
 
 	mux := mux.NewRouter()
-	mux.HandleFunc("/user", see).Methods("POST")
+	mux.HandleFunc("/user", db.PostUser).Methods("POST")
+	mux.HandleFunc("/user/{id:[0-9a-zA-Z]*}", db.GetUser).Methods("GET")
+	mux.HandleFunc("/user/{id:[0-9a-zA-Z]*}", db.DeleteUser).Methods("DELETE")
 	srv := &http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
 		Handler: mux,
 	}
 
