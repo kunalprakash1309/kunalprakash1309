16bb79f2c25a3f10ce9f28e0126c486519ee33f1
kunal prakash <kunalprakash1309@gmail.com>
2021-02-24 19:12:51 +0530

    Added another logging method

--- Go-Kit_Tutorial/main.go
+++ Go-Kit_Tutorial/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	// "log"
-	"context"
+	//"context"
 	"net/http"
 	"os"
 
-	"github.com/go-kit/kit/endpoint"
+	//"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
@@ -24,23 +24,24 @@ func main() {
 	var svc StringService
 
 	svc = stringService{}
+	svc = loggingMiddleware{logger, svc}
 
-	var uppercase endpoint.Endpoint
-	uppercase = makeUppercaseEndpoint(svc)
-	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
+	// var uppercase endpoint.Endpoint
+	// uppercase = makeUppercaseEndpoint(svc)
+	// uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
 
-	var count endpoint.Endpoint
-	count = makeCountEndpoint(svc)
-	count = loggingMiddleware(log.With(logger, "method", "count"))(count)
+	// var count endpoint.Endpoint
+	// count = makeCountEndpoint(svc)
+	// count = loggingMiddleware(log.With(logger, "method", "count"))(count)
 
 	uppercaseHandler := httptransport.NewServer(
-		uppercase,
+		makeUppercaseEndpoint(svc),
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
 
 	countHadler := httptransport.NewServer(
-		count,
+		makeCountEndpoint(svc),
 		decodeCountRequest,
 		encodeResponse,
 	)
@@ -50,14 +51,14 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-type Middleware func(endpoint.Endpoint) endpoint.Endpoint
+// type Middleware func(endpoint.Endpoint) endpoint.Endpoint
 
-func loggingMiddleware(logger log.Logger) Middleware {
-	return func(next endpoint.Endpoint) endpoint.Endpoint {
-		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			logger.Log("msg", "calling endpoint")
-			defer logger.Log("msg", "called endpoint")
-			return next(ctx, request)
-		}
-	}
-}
+// func loggingMiddleware(logger log.Logger) Middleware {
+// 	return func(next endpoint.Endpoint) endpoint.Endpoint {
+// 		return func(ctx context.Context, request interface{}) (interface{}, error) {
+// 			logger.Log("msg", "calling endpoint")
+// 			defer logger.Log("msg", "called endpoint")
+// 			return next(ctx, request)
+// 		}
+// 	}
+// }
